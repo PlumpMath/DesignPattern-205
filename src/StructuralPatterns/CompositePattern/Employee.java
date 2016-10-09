18691d6bfa8ada239fe861a0ae1a8732aded5d4c
shixiang <shixiang.sh@superjia.com>
2016-10-09 17:22:52 +0800

    组合模式 CompositePattern

---
+++ src/StructuralPatterns/CompositePattern/Employee.java
@@ -0,0 +1,39 @@
+package StructuralPatterns.CompositePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/9.
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e){
+        this.subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        this.subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return this.subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
