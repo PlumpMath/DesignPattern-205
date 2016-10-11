1e4dd11329e53a7b71c00df61101226f9adacca9
shixiang <shixiang.sh@superjia.com>
2016-10-11 14:28:48 +0800

    解释器模式 InterpreterPattern

---
+++ src/BehavioralPatterns/InterpreterPattern/Expression.java
@@ -0,0 +1,8 @@
+package BehavioralPatterns.InterpreterPattern;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public interface Expression {
+    public boolean interpret(String context);
+}
